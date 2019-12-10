d370d2925dbbaf6c565c94108beab820d8cde5af
SinLapis <stradust0001@gmail.com>
2019-12-10 16:29:54 +0800

    添加os扫描

--- src/main/kotlin/org/ndp/port_scan/bean/Host.kt
+++ src/main/kotlin/org/ndp/port_scan/bean/Host.kt
@@ -1,8 +1,9 @@
 package org.ndp.port_scan.bean
 
-import org.ndp.port_scan.bean.Port
 
 class Host(
         val address: String,
+        val os: String,
+        val hardware: String,
         val ports: List<Port>
 )
